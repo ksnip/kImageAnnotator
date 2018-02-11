60a33b0f569a73a6111bb896aa9c6590c53e7fa9
Damir Porobic <damir_porobic@live.com>
2018-02-11 19:34:17 +0100

    Initial commit

---
+++ src/KImageAnnotator.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Damir Porobic <damir.porobic@gmx.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KIMAGEANNOTATOR_H
+#define KIMAGEANNOTATOR_H
+
+#include <QWidget>
+#include <QGraphicsView>
+#include <QVBoxLayout>
+
+#include "AnntationArea.h"
+
+class KImageAnnotator : public QWidget
+{
+public:
+    KImageAnnotator(const QPixmap& image);
+    ~KImageAnnotator();
+
+private:
+    AnntationArea* mAnnotationArea;
+    QGraphicsView* mView;
+    QVBoxLayout*   mMainLayout;
+};
+
+#endif // KIMAGEANNOTATOR_H
