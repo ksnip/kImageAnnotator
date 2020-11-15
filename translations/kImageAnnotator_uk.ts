ed7ab35acd6072724d74e8e9bbdfb3ae4387298e
Yuri Chornoivan <yurchor@ukr.net>
2020-11-15 06:52:21 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (65 of 65 strings)
    
    Translation: kImageAnnotator/kImageAnnotator
    Translate-URL: https://hosted.weblate.org/projects/kimageannotator/kimageannotator/uk/

--- translations/kImageAnnotator_uk.ts
+++ translations/kImageAnnotator_uk.ts
@@ -288,7 +288,7 @@
     <name>kImageAnnotator::ZoomIndicator</name>
     <message>
         <source>Zoom Level</source>
-        <translation type="unfinished"></translation>
+        <translation>Масштаб</translation>
     </message>
 </context>
 </TS>
