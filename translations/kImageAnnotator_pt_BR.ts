2900196e8ec4acb18b304a093e526af15bc455be
Damir Porobic <damir_porobic@live.com>
2020-12-13 21:37:24 +0100

    Update translation files

--- translations/kImageAnnotator_pt_BR.ts
+++ translations/kImageAnnotator_pt_BR.ts
@@ -129,11 +129,38 @@
     </message>
     <message>
         <source>Border and Fill</source>
-        <translation>Com borda e com preenchimento</translation>
+        <translation type="unfinished">Com borda e com preenchimento</translation>
     </message>
     <message>
         <source>Border and No Fill</source>
-        <translation>Com borda e sem preenchimento</translation>
+        <translation type="unfinished">Com borda e sem preenchimento</translation>
+    </message>
+    <message>
+        <source>No Border and No Fill</source>
+        <translation type="unfinished">Sem borda e sem preenchimento</translation>
+    </message>
+</context>
+<context>
+    <name>kImageAnnotator::ImageEffectPicker</name>
+    <message>
+        <source>Image Effects</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No Effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Drop Shadow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Grayscale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Border</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>No Border and No Fill</source>
