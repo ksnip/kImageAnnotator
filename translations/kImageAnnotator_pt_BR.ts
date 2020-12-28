103d0f69ced357411fe085696795047cc4c4a5c5
antrrax <antrrax@gmail.com>
2020-12-28 10:28:12 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 98.4% (65 of 66 strings)
    
    Translation: kImageAnnotator/kImageAnnotator
    Translate-URL: https://hosted.weblate.org/projects/kimageannotator/kimageannotator/pt_BR/

--- translations/kImageAnnotator_pt_BR.ts
+++ translations/kImageAnnotator_pt_BR.ts
@@ -68,7 +68,7 @@
     </message>
     <message>
         <source>Number Seed</source>
-        <translation type="unfinished"></translation>
+        <translation>Número</translation>
     </message>
 </context>
 <context>
@@ -285,7 +285,7 @@
     </message>
     <message>
         <source>Duplicate</source>
-        <translation type="unfinished"></translation>
+        <translation>Duplicar</translation>
     </message>
 </context>
 <context>
